2e0516ea03118dfb8fbee5b528be8432affe5f7f
Caverna <91982607+CAVERNASTUDIO@users.noreply.github.com>
2025-12-20 20:59:12 -0600

    Caverna Studio
    ========
    
    JupyterLab/Jupyterbook
    Ing. Erik Alejandro García Aparicio
    ====================== 2025

---
+++ JupyterLab/JupyterLab.bat
@@ -0,0 +1,12 @@
+@echo off
+Color 02
+mode con: cols=100 lines=30
+Title Jupyter Caverna Studio
+echo ==============================================================================================
+echo                                            JupyterLab
+echo ==============================================================================================
+echo                                                           Ing. Erik Alejandro García Aparicio
+echo              copyrigth (c) - 2025 Erik García - All rigths reserved. 
+jupyter lab
+pause
+
