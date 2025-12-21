44c325c430412d9fb10d98049eb66722cf9ff1b9
Caverna <91982607+CAVERNASTUDIO@users.noreply.github.com>
2025-12-21 13:09:52 -0600

    Acceso
    Copyrigth (c) - 2025 Erik García - All rigths reserved. 
    Copyright (c) Jupyter Development Team.
    Distributed under the terms of the Modified BSD License.

--- JupyterLab/JupyterLab.bat
+++ JupyterLab/JupyterLab.bat
@@ -1,11 +1,11 @@
 @echo off
 Color 02
-mode con: cols=100 lines=30
+mode con: cols=150 lines=150
 Title Jupyter Caverna Studio
-echo ==============================================================================================
-echo                                            JupyterLab
-echo ==============================================================================================
-echo                                                           Ing. Erik Alejandro García Aparicio
+echo ================================================================================================================================================
+echo                                                                      JupyterLab
+echo ================================================================================================================================================
+echo                                                                                                              Ing. Erik Alejandro García Aparicio
 echo Copyrigth (c) - 2025 Erik García - All rigths reserved. 
 echo Copyright (c) Jupyter Development Team.
 echo Distributed under the terms of the Modified BSD License.
