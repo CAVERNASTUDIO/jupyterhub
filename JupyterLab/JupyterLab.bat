b73fae9f3c02f27f1054c887f53cbaa2a44660eb
Caverna <91982607+CAVERNASTUDIO@users.noreply.github.com>
2026-01-24 19:30:21 -0600

    LocalApp

--- JupyterLab/JupyterLab.bat
+++ JupyterLab/JupyterLab.bat
@@ -1,14 +1,15 @@
 @echo off
-Color 02
+Color 05
 mode con: cols=150 lines=150
 Title Jupyter Lab 
 echo ================================================================================================================================================
 echo                                                                      JupyterLab
 echo ================================================================================================================================================
 REM User:_
-echo _                                                                                                                 
+echo _  
 echo Copyright (c) Jupyter Development Team.
 echo Distributed under the terms of the Modified BSD License.
+echo [E %DATE% %TIME% LocalApp]
 jupyter lab
 pause
 
@@ -28,3 +29,4 @@ pause
 
 
 REM copyright (c) - 2026 Erik Alejandro García Aparicio. All rights reserved.
+
